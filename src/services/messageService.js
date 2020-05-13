176ca0d8ccaed02bc17193e44e07fa59b73a4353
Lucas Parzianello <lucaspar@users.noreply.github.com>
2020-05-13 00:56:03 -0400

    front - new api methods

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,6 +1,7 @@
 import api from '@/services/api'
 
 export default {
+
     fetchMessages() {
         return api.get(`messages/`)
             .then(response => response.data)
@@ -12,5 +13,23 @@ export default {
     deleteMessage(msgId) {
         return api.delete(`messages/${msgId}`)
             .then(response => response.data)
-    }
+    },
+
+    // annotator methods:
+    fetchImageList(sessionUUID) {
+        return api.get(`images/list`, {
+            sessionUUID,
+        }).then(response => response.data)
+    },
+    fetchImage(sessionUUID, imgID) {
+        return api.get(`images/${imgID}`, {
+            sessionUUID,
+        }).then(response => response.data)
+    },
+    postAnnotation(sessionUUID, imgID) {
+        return api.post(`images/${imgID}`, {
+            sessionUUID,
+        }).then(response => response.data)
+    },
+
 }
