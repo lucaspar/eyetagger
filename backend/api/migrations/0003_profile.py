84ec05d94556905906327f17d1e3894dab961963
Lucas Parzianello <lucaspar@users.noreply.github.com>
2020-07-13 20:16:52 -0400

    removed dataset-specific migration

--- backend/api/migrations/0003_profile.py
+++ backend/api/migrations/0003_profile.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api', '0002_auto_20200519_2239'),
+        ('api', '0001_initial'),
     ]
 
     operations = [
